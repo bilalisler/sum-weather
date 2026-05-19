da25d22b7b5e1656c8908ad98a9a6e79f76b05af
Bilal İŞLER <mercan_shark@windowslive.com>
2026-05-19 08:14:47 +0300

    test

--- sum.js
+++ sum.js
@@ -3,9 +3,9 @@ const qrcode = require('qrcode-terminal');
 const cron = require('node-cron');
 const axios = require('axios');
 
-const SEHIR = 'bursa';
-const TELEFON_NUMARASI = '905319161340'; //905319161340   905387672037
-//const GONDERIM_SAATI = '30 07 * * *';  
+const SEHIR = 'istanbul';
+const TELEFON_NUMARASI = '905387672037'; //905319161340   905387672037
+//const GONDERIM_SAATI = '30 07 * * *';
 const GONDERIM_SAATI = '* * * * *';
 const HEDEF_TARIH = '2026-06-06';
 
@@ -27,7 +27,6 @@ client.on('ready', () => {
 });
 
 const evlilikMesajlari = [
-
     {
         tur: "💬 Hadis-i Şerif",
         metin: "«Evlilik benim sünnetimdir. Kim benim sünnetimle amel etmezse benden değildir. Evleniniz, çünkü ben diğer ümmetlere karşı sizin çokluğunuzla iftihar edeceğim.»",
@@ -51,15 +50,15 @@ const evlilikMesajlari = [
 ];
 
 
-function dailyMessage() {
+function dailyHadith() {
     const randIndex = Math.floor(Math.random() * evlilikMesajlari.length)
     return evlilikMesajlari[randIndex];
 }
 
-function dailyCounterMessage() {
+function meetDayCounter() {
     const hedef = new Date(HEDEF_TARIH);
-    const bugun = new Date();
-    const bugununTarihi = new Date(bugun.getFullYear(), bugun.getMonth(), bugun.getDate()); // Saat farkından etkilenmemek için
+    const today = new Date();
+    const bugununTarihi = new Date(today.getFullYear(), today.getMonth(), today.getDate()); // Saat farkından etkilenmemek için
     const hedefTarihYalin = new Date(hedef.getFullYear(), hedef.getMonth(), hedef.getDate());
 
     const farkZaman = hedefTarihYalin - bugununTarihi;
@@ -77,49 +76,83 @@ function dailyCounterMessage() {
     return geriSayimMesaji
 }
 
+function selectOutfitSuggestion(temprature) {
+    let outfitSuggestion = "";
+
+    if (temprature < 5) {
+        outfitSuggestion = "🥶 *Lahanaya dönme vakti!* Hava buz gibi. En kalın montunu giy, atkı ve bereni sakın evde unutma.";
+    } else if (temprature >= 5 && temprature < 12) {
+        outfitSuggestion = "js 🧥 *Klasik Kış Modu.* Kalın kazaklar ve kışlık kabanın günü. Ayaklarını sıcak tutacak botları tercih et.";
+    } else if (temprature >= 12 && temprature < 18) {
+        outfitSuggestion = "🧥 *Akıllı Katmanlar!* Bu hava adamı aldatır. Kat kat giyin ki öğlen sıcaklarsan çıkarasın. Deri ceket veya trençkot harika olur.";
+    } else if (temprature >= 18 && temprature < 24) {
+        outfitSuggestion = "👚 *Tatlı Bahar Esintisi.* Hafif bir sweatshirt, denim ceket veya uzun kollu şık bir gömlek tam bugünün havası.";
+    } else {
+        outfitSuggestion = "☀️ *Tiril Tiril Yaz!* Hava harika. Tişörtünü, keten pantolonunu veya en sevdiğin elbiseni giy. Güneş gözlüğünü unutma!";
+    }
+
+    return outfitSuggestion
+}
+
+function alarm(rainfall, uvIndex) {
+    // 🌧️ Akıllı Yağmur ve 🧴 UV Kremi Alarmları
+    let smartNotes = "";
+    if (rainfall > 0.1) {
+        smartNotes += "\n⚠️ *Önemli:* Bugün yağmur geçişleri görünüyor, yanına mutlaka şemsiye alman iyi olabilir! 🌧️";
+    }
+    if (uvIndex >= 3) {
+        smartNotes += "\n☀️ *Cilt Bakımı:* Bugün UV indeksi biraz yüksek, güneş kremini sürmeden evden çıkma lütfen.";
+    }
+
+    return smartNotes
+}
+
 async function kombinGonder() {
     try {
         const url = `https://wttr.in/${SEHIR}?format=j1`;
         const cevap = await axios.get(url);
         const veri = cevap.data;
 
-        const sicaklik = parseInt(veri.current_condition[0].temp_C);
-        const havaDurumu = veri.current_condition[0].weatherDesc[0].value;
+        const temprature = parseInt(veri.current_condition[0].temp_C); // gerçek sıcaklık
+        const weather = veri.current_condition[0].weatherDesc[0].value; // hava durumu
+        const perceivedTemperature = parseInt(veri.current_condition[0].FeelsLikeC); // Hissedilen Sıcaklık
+        const rainfall = parseFloat(veri.current_condition[0].precipMM); // Yağış Miktarı
+        const uvIndex = parseInt(veri.current_condition[0].uvIndex); // UV İndeksi
 
-        let kombinOnerisi = "";
+        let outfitSuggestion = selectOutfitSuggestion(temprature);
+        
+        let smartNotes = alarm(rainfall, uvIndex)//🌧️ Akıllı Yağmur ve 🧴 UV Kremi Alarmları
+        const hadith = dailyHadith(); // günlük hadis
+        const counterMessage = meetDayCounter();  // buluşma günü sayacı
 
-        if (sicaklik < 5) {
-            kombinOnerisi = "🥶 *Lahanaya dönme vakti!* Hava buz gibi. En kalın montunu giy, atkı ve bereni sakın evde unutma.";
-        } else if (sicaklik >= 5 && sicaklik < 12) {
-            kombinOnerisi = "js 🧥 *Klasik Kış Modu.* Kalın kazaklar ve kışlık kabanın günü. Ayaklarını sıcak tutacak botları tercih et.";
-        } else if (sicaklik >= 12 && sicaklik < 18) {
-            kombinOnerisi = "🧥 *Akıllı Katmanlar!* Bu hava adamı aldatır. Kat kat giyin ki öğlen sıcaklarsan çıkarasın. Deri ceket veya trençkot harika olur.";
-        } else if (sicaklik >= 18 && sicaklik < 24) {
-            kombinOnerisi = "👚 *Tatlı Bahar Esintisi.* Hafif bir sweatshirt, denim ceket veya uzun kollu şık bir gömlek tam bugünün havası.";
+        // Hafta Sonu Kontrolü
+        const today = new Date();
+        const isWeekend = [0, 6].includes(today.getDay());
+
+        let message = ""
+        if (isWeekend) {
+            message = 'Günaydın! ☀️ Harika bir gün geçirmen dileğiyle.';
         } else {
-            kombinOnerisi = "☀️ *Tiril Tiril Yaz!* Hava harika. Tişörtünü, keten pantolonunu veya en sevdiğin elbiseni giy. Güneş gözlüğünü unutma!";
+            message = `Hayırlı Sabahlar! ☀️\n\n` +
+                `📍 *Konum:* ${SEHIR.toUpperCase()}\n` +
+                `🌡️ *Hissedilen Hava:* ${perceivedTemperature}°C (Gerçek: ${temprature}°C - ${weather})\n\n` +
+                `👗 *Bugünkü Kombin Reçeten:*\n${outfitSuggestion}${smartNotes}\n\n` +
+                `*${hadith.tur}:*\n${hadith.metin}\n_(${hadith.kaynak})_\n\n` +
+                `${counterMessage}\n\nHarika bir gün geçirmen dileğiyle! ✨`;
         }
 
-        const rastgeleManeviyat = dailyMessage();
-        const counterMessage = dailyCounterMessage();
-
-        const mesaj = `Hayırlı Sabahlar! ☀️\n\n📍 *Konum:* ${SEHIR.toUpperCase()}\n🌡️ *Hava:* ${sicaklik}°C (${havaDurumu})\n\n👗 *Bugünkü Kombin Reçeten:*\n${kombinOnerisi}\n\n━━━━━━━━━━━━━━━\n\n💝 *Günün Ayet/Hadisi*\n*${rastgeleManeviyat.tur}:*\n${rastgeleManeviyat.metin}\n_(${rastgeleManeviyat.kaynak})_\n\n━━━━━━━━━━━━━━━\n\n${counterMessage}_\n\nHarika bir gün geçir! ✨`;
-
         const chatId = `${TELEFON_NUMARASI}@c.us`;
-
-
-        await client.sendMessage(chatId, mesaj);
+        await client.sendMessage(chatId, message);
         console.log(`🚀 Mesaj başarıyla gönderildi: ${new Date().toLocaleTimeString()}`);
 
     } catch (hata) {
-        console.error('❌ Hava durumu veya mesaj gönderme hatası:', hata);
+        console.error('❌ Hava durumu veya message gönderme hatası:', hata);
     }
 }
 
 cron.schedule(GONDERIM_SAATI, () => {
- console.log('⏰ Zamanı geldi, hava durumu kontrol ediliyor...');
- kombinGonder();
- });
-
-client.initialize();
+    console.log('⏰ Zamanı geldi, hava durumu kontrol ediliyor...');
+    kombinGonder();
+});
+client.initialize()
 
